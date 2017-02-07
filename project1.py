4abd797ade5729bd7e62f1092fb47fae7720908f
Michael <mikewtrs66@gmail.com>
2017-02-07 19:44:57 +0000

    Made significant changes and progress, still lost

--- project1.py
+++ project1.py
@@ -1,2 +1,12 @@
-print("Faster!")
-# Commenting here in gray text.
+from PIL import Image
+img1 = Image.open("Images/1.png")
+img2 = Image.open("Images/2.png")
+img3 = Image.open("Images/3.png")
+img4 = Image.open("Images/4.png")
+img5 = Image.open("Images/5.png")
+img6 = Image.open("Images/6.png")
+img7 = Image.open("Images/7.png")
+img8 = Image.open("Images/8.png")
+img9 = Image.open("Images/9.png")
+
+work1 = Image.getpixel(("1.png")) 
--- text.py
+++ text.py
@@ -1,4 +1,1 @@
-from PIL import Image
-print ("It's not him, there must have been a mistake.")
-art = Image.open("Images/1.png")
-print(art.format)
+print("Hi!")
