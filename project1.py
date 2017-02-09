06be9489f80a5a889f13193d43361eb823577ba1
Michael <mikewtrs66@gmail.com>
2017-02-09 04:35:53 +0000

    Project is finished

--- project1.py
+++ project1.py
@@ -1,12 +1,48 @@
 from PIL import Image
-img1 = Image.open("Images/1.png")
-img2 = Image.open("Images/2.png")
-img3 = Image.open("Images/3.png")
-img4 = Image.open("Images/4.png")
-img5 = Image.open("Images/5.png")
-img6 = Image.open("Images/6.png")
-img7 = Image.open("Images/7.png")
-img8 = Image.open("Images/8.png")
-img9 = Image.open("Images/9.png")
 
-work1 = Image.getpixel(("1.png")) 
+def medianOdd(myList):
+ listLength = len(myList)
+ sortedValues = sorted(myList)
+ middleIndex = ((listLength + 1)/2) - 1
+ return sortedValues[middleIndex]
+
+img = []
+img.append(Image.open("1.png"))
+img.append(Image.open("2.png"))
+img.append(Image.open("3.png"))
+img.append(Image.open("4.png"))
+img.append(Image.open("5.png"))
+img.append(Image.open("6.png"))
+img.append(Image.open("7.png"))
+img.append(Image.open("8.png"))
+img.append(Image.open("9.png"))
+
+pWidth, pHeight = img[0].size
+print pHeight, pWidth
+rPixel = []
+gPixel = []    #To clear a list, re-initializa it.
+bPixel = []
+image = Image.new("RGB", (pWidth, pHeight), "white") 
+
+for x in range(pWidth):
+ for y in range(pHeight):
+  for myImg in img:  #<----What does this do? In?
+   myRed, myGreen, myBlue = myImg.getpixel((x,y))
+   
+   rPixel.append(myRed)
+   gPixel.append(myGreen)
+   bPixel.append(myBlue)
+  
+  aRedmedian = medianOdd(rPixel)
+  aGrnmedian = medianOdd(gPixel)
+  aBlumedian = medianOdd(bPixel)
+  image.putpixel((x, y), (aRedmedian, aGrnmedian, aBlumedian))
+  rPixel = []
+  gPixel = [] #reinitialized!
+  bPixel = []
+
+
+image.save("Monument.png")  
+
+#Apply the median filter
+#Insert new pixels into new image <img.putpixel(xy, median value)>
